75491ff9cc3ade6c35e97128eec7c65913ced914
nikostsiftis <tcad.center@gmail.com>
2014-09-23 07:38:19 +0300

    Update clitutorial.js

--- clitutorial.js
+++ clitutorial.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+var program = require('commander');
+
+program
+  .version('0.0.1');
+  
+program
+  .command('hi')
+  .description('intialize project configuration')
+  .action(function(){
+    console.log('Hi my Friend!!!');
+  });
+  
+program
+  .command('bye [name]')
+  .description('initialize project configuration')
+  .action(function(name){
+    console.log('Bye' + name + '. It was good to see you!');
+  });
+  
+program 
+  .command('*')
+  .action(function(){
+    console.log('Enter a Valid command');
+  });
+  
+program.parse(process.argv);
